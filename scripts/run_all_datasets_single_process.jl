008b24e7f156daec7d405d2007d1e46083933278
Felippe Alves <flipgm@gmail.com>
2023-12-18 09:01:10 +0000

    Tweaking the script

--- scripts/run_all_datasets_single_process.jl
+++ scripts/run_all_datasets_single_process.jl
@@ -9,8 +9,11 @@ using Chain, DataFrames, Dates, NamedTupleTools, Parquet2, StatsBase, Transducer
 include(srcdir("utils.jl"))
 include(srcdir("pipeline_functions.jl"))
 
-function run_all_datasets_single_process(number_of_sequences=3, k=2, sequence_length=5; niter=10, decfunc=GBPAlgorithm.decode)
+datadilename(n,k,T;prefix="") = "$(prefix)_nseq_$(n)_k_$(k)_T_$(T)_results.parquet"
+
+function run_all_datasets_single_process(number_of_sequences=3, k=2, sequence_length=5; niter=10, decfunc=GBPAlgorithm.decode, dir="",prefix="")
 	t0 = now()
+    mkpath(datadir("exp_pro",dir))
     @info "Starting $(t0)"
     r = all_dataset_params(sequence_length,number_of_sequences,k)|>
 		MapCat() do params
@@ -18,16 +21,16 @@ function run_all_datasets_single_process(number_of_sequences=3, k=2, sequence_le
 				foldxl(vcat)	
 		end|>
 		Map(decoding_results(niter,decfunc))|>
-		# collect|>
 		DataFrame|>
 		tidy_results|>
-        savedata(datadir("exp_pro","nseq_$(number_of_sequences)_k_$(k)_T_$(sequence_length)_results.parquet"))
+        savedata(datadir("exp_pro",dir,datadilename(number_of_sequences,k,sequence_length;prefix)))
     tf = now()
     @info "Finished $(tf). Took $(tf - t0)"
+    return r
 end
 
 @info "Running a smaller batch to compile"
-run_all_datasets_single_process();
+run_all_datasets_single_process(;prefix="test");
 @info "... one more..."
-run_all_datasets_single_process();
-@info "Done. Run `run_all_datasets_single_process(nseq,k,T;niter,decfunc)` with the desired parameters." 
+run_all_datasets_single_process(;prefix="test");
+@info "Done. Run `run_all_datasets_single_process(nseq,k,T;niter,decfunc,dir,prefix)` with the desired parameters." 
